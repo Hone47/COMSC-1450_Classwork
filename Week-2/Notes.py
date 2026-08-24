b8685dc216b474521ed31afc82da9dd03159a1f8
danny.hoa09 <danny.hoa@gmail.com>
2026-08-24 14:56:18 -0500

    Class 2 week 2

---
+++ Week-2/Notes.py
@@ -0,0 +1,47 @@
+#types in python 
+#int, float, str. bool 
+#to see the type of you variable use func type()
+#stings concating with '+' 
+
+
+#type conversion in python 
+#int() , float(), str(), 
+print("Hello to the python class")
+print(type(45))
+print(type(45.98))
+print(type("OKAY"))
+
+print(5+6)
+
+print("Hello"+"python")
+
+print("hello" + str(2026))
+
+x = 3
+y = int(float(x))
+
+#doesnt round only keeps the first decimal point.
+print(type(x))
+print(y) 
+
+x = 5
+y = 5
+z = 5
+
+x=y=z=5
+print(x)
+print(y)
+print(z)
+
+a = 1 
+b = 2 
+c = 3
+
+a , b , c = 1, 2, 3
+
+
+print("Welcome to the python class")
+print("Fall")
+print("2026")
+
+print("Welcom to the python\nFall\n2026")
