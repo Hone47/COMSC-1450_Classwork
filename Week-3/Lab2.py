557bc25c0a25f5d9d786e5a97963aced6ea477a0
danny.hoa09 <danny.hoa@gmail.com>
2026-09-03 00:34:55 -0500

    lab2

--- Week-3/Lab2.py
+++ Week-3/Lab2.py
@@ -77,6 +77,44 @@ elif(grade<70 and grade>69):
  print(grade, "is D")
 else:
   print(grade,"is F")
-
-
   
+  
+  
+
+number1 = int(input("Enter your number"))
+number2 = int(input("Enter your number"))
+number3 = int(input("Enter your number"))
+
+if(number1 >number2 and number1 > number3):
+  print(number1,"is greater")
+elif(number2 > number1 and number2 > number3):
+  print(number2,"is greater")
+elif(number3 > number1 and number3 > number2):  
+  print(number3,"is greater")
+else:
+  print("number are the same")
+
+
+age = int(input("Enter your age"))
+if(age < 13):
+  print("price is 5 dollars")
+elif(age <13 and age > 18):
+  print("price is 8 dollars")
+elif(age < 65  and age >= 18):
+  print("Adult")
+elif(age <=65):
+ print("price is 7 dollars")
+
+number1 = int(input("Enter your number"))
+number2 = int(input("Enter your number"))
+operator = (input("Enter your operator +,-,*,/"))
+
+if(operator == "+"):
+  print(number1+number2)
+if(operator == "-"):
+  print(number1-number2)
+if(operator == "*"):
+  print(number1/number2)
+if(operator == "/"):
+  print(number1/number2)
+
